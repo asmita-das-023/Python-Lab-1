10d7287fce0631894b2988722e7e56c33035cdb9
asmita-das-023 <asmitadasmail@yahoo.com>
2026-08-17 21:25:26 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,16 @@
+#Mathematical Calculator using Built-in Functions and Math Module 
+
+#Develop a Python program that performs various mathematical operations using arithmetic operators, built-in functions, and the math module. 
+
+import math
+number = float(input("Enter a number: "))
+print("\n----- Mathematical Operations -----")
+print(f"Number          : {number}")
+print(f"Square          : {number**2}")
+print(f"Cube            : {number ** 3}")
+print(f"Square Root     : {math.sqrt(number)}")
+print(f"Ceiling Value   : {math.ceil(number)}")
+print(f"Floor Value     : {math.floor(number)}")
+print(f"Absolute Value  : {abs(number)}")
+print(f"Type            : {type(number)}")
+print(f"Memory Address  : {id(number)}")
